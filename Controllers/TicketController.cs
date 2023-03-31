383d03c9e8a109a89543e10b515e9910bd8b73ba
rokgali <galinauskasrokas@gmail.com>
2023-03-31 11:26:52 +0300

    Added ticket creation method

--- Controllers/TicketController.cs
+++ Controllers/TicketController.cs
@@ -1,6 +1,9 @@
-﻿using bugtrackerback.Entities.DTOS;
+﻿using bugtrackerback.Areas.Identity.Data;
+using bugtrackerback.Entities;
+using bugtrackerback.Entities.DTOS;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace bugtrackerback.Controllers
 {
@@ -8,12 +11,42 @@ namespace bugtrackerback.Controllers
     [ApiController]
     public class TicketController : ControllerBase
     {
+        private readonly bugtrackerdbContext _context;
+
+        public TicketController(bugtrackerdbContext context)
+        {
+            _context = context;
+        }
         [HttpPost]
         public async Task<IActionResult> CreateTicket(CreateTicketDTO createTicketDTO)
         {
+            var ticketAuthor = _context.Users.FirstOrDefault(u => u.Email == createTicketDTO.AuthorEmail);
+            var project = _context.Projects.FirstOrDefault(p => p.Id == createTicketDTO.ProjectId);
+            var assignedUsers = await _context.Users.Where(u => createTicketDTO.UserIds.Contains(u.Id)).ToListAsync();
+
+            if(ticketAuthor != null && project != null)
+            {
+                Ticket ticketResult = new Ticket()
+                {
+                    Title = createTicketDTO.Name,
+                    Description = createTicketDTO.Description,
+                    Priority = createTicketDTO.Priority,
+                    Type = createTicketDTO.Type,
+                    Status = createTicketDTO.Status,
+                    AuthorId = ticketAuthor.Id,
+                    Comments = null,
+                    Users = assignedUsers,
+                    Project = project
+                };
+
+                var result = await _context.Tickets.AddAsync(ticketResult);
+                _context.SaveChanges();
+
+                return Ok("Success!");
+            }
 
 
-            return Ok();
+            return BadRequest("Failed to create ticket");
         }
     }
 }
